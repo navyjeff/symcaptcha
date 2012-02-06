c9ed8e3abbca1276c8f35fae2ee241fd6034e9c2
unknown <Jeffrey@.(none)>
2012-02-06 16:44:48 -0500

    3rd attempt to create SQLite data file. No luck so far.

--- phpinfo.php
+++ phpinfo.php
@@ -1,5 +1,10 @@
 <?php
 
+  //create or open the database
+  $database = new SQLiteDatabase('symcaptcha.sq3', 0666, $error);
+
+  $query = 'CREATE TABLE Movies ' .
+         '(Title TEXT, Director TEXT, Year INTEGER)';
 phpinfo()
 
 ?>
